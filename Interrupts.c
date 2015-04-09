dc7bbb51e9a820785d4761ebd20e7db1c7720b77
Jordan Millington <jordanm1653@gmail.com>
2015-04-09 15:57:16 -0400

    Interrupts.c: Clean up
    Cleaned code and comments

--- Interrupts.c
+++ Interrupts.c
@@ -1,54 +1,87 @@
-/******************************************************************
- *	Interrupts
- *--------------------
- *	Jordan Millington
- *	2013-04-22
+/*
+ * Interrupts.c
  *
- *****************************************************************/	
+ * Demonstraton code describing the process of using interrupts
+ * 
+ * CHIP: PIC 18F4685
+ * COMPILER: mplab c18
+ *
+ * DATE CREATED: 04.22.2013
+ * AUTHOR: Jordan Millington
+ */
 
-#include <p18f4685.h>				//Tell compiler which chip I am using 
+#include <p18f4685.h>	/* Chip Library */
 
-
+/*
+ * Pre-proccessor Configurations
+ * @ MCLRE - Master clear enable
+ * @ WDT - Watch dog timer
+ * @ OSC - Oscillator
+ * @ LVP - Low voltage programming
+ * @ PBADEN - Port B analog to digital enable
+ */
 #pragma config MCLRE=OFF, WDT=OFF, OSC=IRCIO67, LVP=OFF, PBADEN=OFF
 
+/*
+ * Interrupt Service Routine
+ * Any interrupt will be directed here
+ */
+#pragma interrupt my_isr
+void my_isr (void)
+{
 
+	/* Check for Port B interrupt flag (RBIF) */
+	if(INTCONbits.RBIF==1){
 
-#pragma interrupt my_isr				//"My Interrupt Service Routine
-void my_isr (void){						//Handle the interrupt
-//When any interrupt occurs the code comes here. Here you will need to determine
-//which flag cause the interrupt and handle it from there.
-if(INTCONbits.RBIF==1){
+		//Some Code
 
-	//Some Code
-	
-	INTCONbits.RBIF=0;			//Must be cleared in software
+		/*
+		 * Clear interrupt flag
+		 */
+		INTCONbits.RBIF=0;
 	}
 }
 
-
-#pragma code high_vector=0x0008		//Non-changeable intterupt vector at 0x0008
+/* 
+ * Pre-processor declaration of interrupt vector
+ * memory address: 0x0008
+ */
+#pragma code high_vector=0x0008
 void my_hivect_int (void)
-	{
-	//Use an assembly command to save space in memory at 0x0008
+{
+	/*
+	 * Inline assembly to save space in memory
+	 * GOTO interrupt service routine
+	 */
 	_asm
-	GOTO my_isr						//Go to where i want to handle the interrupt
+	GOTO my_isr
 	_endasm
+}
 
-	}
-
+/*
+ * Pre-processor - Declare beginning of code
+ */
 #pragma code
 
-void main (void){
-	
-	INTCON2bits.RBPU=0;				//Enable PORTB pull up resistors
-	INTCONbits.RBIE=1;				//Enable the PORTB change interrupt
-	INTCONbits.PEIE=1;				//Enable the periphrial interrupts
-	INTCONbits.GIE=1;				//Enable all (global) interrupts
+void main (void)
+{
+	/*
+	 * INTERRUPT INITIALIZATION
+	 * INTCON Register:
+	 * @ RBIE:	Enable PORTB change interrupts
+	 * @ PEIE:	Enable peripheral inturrupts
+	 * @ GIE:	Enable global interrupts
+	 * INTCON2 Register:
+	 * @ RBPU:	Enable pull up resistors
+	 */
+	INTCONbits.RBIE=1;
+	INTCONbits.PEIE=1;
+	INTCONbits.GIE=1;
+	INTCON2bits.RBPU=0;
 
 	while(1){
-	
-	//Some Code
-	
-	}	
+
+		/* Some Code */
+	}
 	while(1);
 }
